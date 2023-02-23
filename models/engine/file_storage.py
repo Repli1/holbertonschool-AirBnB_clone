0ecfc4274e0b438e7821966bdc4e0353f6b537f3
Repli1 <cladescandor42@gmail.com>
2023-02-23 09:51:30 -0800

    Add filestorage file

---
+++ models/engine/file_storage.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""""""
+import json
+
+
+class FileStorage:
+    """"""
+    def __init__(self):
+        with open(filename, 'w') as file:
+            json.dump(self.__objects, self.__file_path)
+
+    def all(self):
+
+    def new(self, obj):
+
+    def save(self):
+
+    def reload(self):
