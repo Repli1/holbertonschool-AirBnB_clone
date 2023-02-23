1ac68fdb500038b82b6286dbc5a2bda019a4cd2b
Fedejalife99 <fedejalife@gmail.com>
2023-02-23 07:53:57 -0800

    task 4 and documentation

--- models/base_model.py
+++ models/base_model.py
@@ -1,19 +1,33 @@
 #!/usr/bin/python3
 import uuid
 import datetime
+"""define a class Base model"""
 
 
 class BaseModel:
-    def __init__(self):
-        no_str_id = uuid.uuid4
-        id_str = str(no_str_id)
-        created_at = datetime.now()
-        updated_at = datetime.now()
+    """initializate """
+    def __init__(self, *args, **kwargs):
+        if kwargs is not None and len(kwargs) > 0:
+            for key, v in kwargs.items():
+                if key == 'id':
+                    id_str = str(v)
+                if key == 'update_at':
+                    update_at = v.isoformat()
+                if key == 'created_at':
+                    created_at = v.isoformat()
+
+        else:
+            no_str_id = uuid.uuid4
+            id_str = str(no_str_id)
+            created_at = datetime.now()
+            updated_at = datetime.now()
 
     def save(self):
+        """update the atribute update_at"""
         update_at = datetime.now()
 
     def to_dict(self):
+        """return a dict from the instance"""
         instance_dict = self.__dict__.copy()
         instance_dict['__class__'] = self.__class__.__name__
         instance_dict['created_at'] = (self.created_at).isoformat()
