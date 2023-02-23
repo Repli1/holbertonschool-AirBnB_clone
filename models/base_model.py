8cb9676820d0fae3491e0e43084bcf819fb32b75
Fedejalife99 <fedejalife@gmail.com>
2023-02-23 08:20:20 -0800

    Fix attribute names and str return syntax

--- models/base_model.py
+++ models/base_model.py
@@ -10,20 +10,20 @@ class BaseModel:
         if kwargs is not None and len(kwargs) > 0:
             for key, v in kwargs.items():
                 if key == 'id':
-                    id_str = str(v)
+                    self.id = str(v)
                 if key == 'update_at':
-                    update_at = v.isoformat()
+                    self.update_at = v.isoformat()
                 if key == 'created_at':
-                    created_at = v.isoformat()
+                    self.created_at = v.isoformat()
         else:
             no_str_id = uuid.uuid4()
-            self.id_str = str(no_str_id)
+            self.id = str(no_str_id)
             self.created_at = datetime.datetime.now()
             self.updated_at = datetime.datetime.now()
 
     def save(self):
         """update the atribute update_at"""
-        update_at = datetime.datetime.now()
+        self.update_at = datetime.datetime.now()
 
     def to_dict(self):
         """return a dict from the instance"""
@@ -34,4 +34,4 @@ class BaseModel:
         return instance_dict
 
     def __str__(self):
-        return f"[<{self.__class__.__name__}>] (<{self.id_str}>) <{self.__dict__}>"
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
