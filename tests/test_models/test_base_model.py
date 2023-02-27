7b0f62d251a475984fa2e70b57cac7f7062a058c
Repli1 <cladescandor42@gmail.com>
2023-02-27 03:43:31 -0800

    Add base model unittests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,5 +1,34 @@
 #!/usr/bin/python3
 import unittest
+from models.base_model import BaseModel
+
 
 class TestBaseModel(unittest.TestCase):
+    def test_save(self):
+        bm = BaseModel()
+        old_date = bm.updated_at
+        bm.save()
+        new_date = bm.updated_at
+        self.assertNotEqual(old_date, new_date)
 
+    def test_to_dict(self):
+        bm = BaseModel()
+        new_dict = bm.to_dict()
+        self.assertEqual(type(new_dict), dict)
+
+    def test_id(self):
+        bm = BaseModel()
+        bm2 = BaseModel()
+        self.assertEqual(type(bm.id), str)
+        self.assertNotEqual(bm.id, bm2.id)
+
+    def test_created_at(self):
+        bm = BaseModel()
+        bm2 = BaseModel()
+        self.assertNotEqual(bm.created_at, bm2.created_at)
+
+    def test_str(self):
+        bm = BaseModel()
+        str1 = bm.__str__()
+        str2 = f"[{bm.__class__.__name__}] ({bm.id}) {bm.__dict__}"
+        self.assertEqual(str1, str2)
